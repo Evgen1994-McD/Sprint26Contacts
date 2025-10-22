a4dcff98ee102cd0b59f4261f6031a0c31b70ce4
Evgen1994-McD <mrmarwell@gmail.com>
2025-10-23 01:13:54 +0500

    Тз выполнено

--- app/src/main/java/com/example/sprint26contacts/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sprint26contacts/presentation/MainActivity.kt
@@ -12,16 +12,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,15 +40,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Sprint26ContactsTheme {
+                ContactDetails(
+                    contact = Contact(
+                        name = "Евгений",
+                        surname = "Сергеевич",
+                        familyName = "Авдейкин",
+                        imageRes = R.drawable.orig,
+                        isFavorite = true,
+                        phone = "+79655631039",
+                        address = "г. Пермь, ул. Космонавта Беляева 61В, кв. 4",
+                        email = "MrMarwell@Gmail.com"
+                    )
+                )
 
             }
         }
     }
 }
-
-
 @Composable
-fun MainScreen(contact: Contact) {
+fun ContactDetails(contact: Contact) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -53,127 +68,160 @@ fun MainScreen(contact: Contact) {
         Avatar(contact)
         Name(contact)
         NumberPhone(contact)
-        Address(contact)
+        AdressPrinter(contact.address, stringResource(R.string.address))
+        AdressPrinter(contact.email, stringResource(R.string.email))
 
     }
-
 }
-
-
 @Composable
 fun Avatar(contact: Contact) {
+    var img = contact.imageRes
     Box {
         Image(
             modifier = Modifier
-                .align(Alignment.Center),
-            painter = painterResource(id = R.drawable.circle), contentDescription = "icon"
+                .align(Alignment.Center)
+                .size(64.dp)
+                .clip(shape = CircleShape),
+            painter = if (img != null) {
+                painterResource(id = img)
+            } else {
+                painterResource(id = R.drawable.circle)
+            },
+            contentDescription = null
         )
-        Text(
-            modifier = Modifier
-                .align(Alignment.Center),
-            text = contact.name.take(1) + contact.familyName.take(1)
-        )
-    }
-
-}
-
-
-@Composable
-fun Address(contact: Contact){
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 10.dp),
-        horizontalArrangement = Arrangement.Center
-    ) {
-Text(
-    text = "Адрес:",
-    modifier = Modifier,
-    fontSize = 18.sp
-)
-        Box(
-            modifier = Modifier
-                .width(150.dp)
-        ){
+        if (img == null) {
             Text(
-                text = contact.address,
-                maxLines = 2
+                modifier = Modifier
+                    .align(Alignment.Center),
+                text = contact.name.take(1) + contact.familyName.take(1)
             )
         }
+    }
 
-
+}
+@Composable
+fun AdressPrinter(adress: String?, text: String?) {
+    if (adress != null) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text.toString(),
+                modifier = Modifier,
+                fontSize = 18.sp
+            )
+            Box(
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(start = 8.dp)
+            ) {
+                Text(
+                    text = adress.toString(),
+                    maxLines = 2
+                )
+            }
+        }
     }
 }
-
-
-
-
 @Composable
-fun NumberPhone(contact: Contact){
+fun NumberPhone(contact: Contact) {
     var number = "---"
-    if (contact.phone.isNotEmpty()){
+    if (contact.phone.isNotEmpty()) {
         number = contact.phone
     }
-Row(
-    modifier = Modifier
-        .padding(top = 40.dp)
-        .fillMaxWidth(),
-    horizontalArrangement = Arrangement.Center
-) {
-    Text(
-        text = "Телефон:",
+    Row(
         modifier = Modifier
-            .padding(end = 3.dp),
-        fontSize = 18.sp
-    )
-    Text(
-        text = number,
-        fontSize = 18.sp
-    )
+            .padding(top = 40.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.phone),
+            modifier = Modifier
+                .padding(end = 3.dp),
+            fontSize = 18.sp
+        )
+        Text(
+            text = number,
+            fontSize = 18.sp
+        )
+    }
 }
-
-}
-
-
-
 @Composable
 fun Name(contact: Contact) {
-    Box(
+    Column(
         modifier = Modifier
-            .padding(top = 10.dp)
+            .padding(top = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             contact.name + " " + contact.surname,
             maxLines = 2,
-            modifier = Modifier,
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold
         )
-        Text(
-            text = contact.familyName,
-            modifier = Modifier
-                .padding(top = 25.dp)
-                .align(Alignment.Center),
-            fontSize = 24.sp,
-
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = contact.familyName,
+                modifier = Modifier
+                    .padding(top = 8.dp),
+                fontSize = 24.sp,
             )
+            if (contact.isFavorite) {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "favorite",
+                    tint = Color.Red,
+                    modifier = Modifier
+                        .padding(
+                            top = 8.dp,
+                            start = 5.dp
+                        )
+                )
+            }
+        }
+
     }
 }
-
+/**
+ * Функции предпросмотра с данными и без данных
+ */
 
 @Preview(showSystemUi = true)
 @Composable
-private fun MainPreview() {
-    MainScreen(
+private fun MainPreviewFullData() {
+    ContactDetails(
         contact = Contact(
             name = "Евгений",
             surname = "Сергеевич",
             familyName = "Авдейкин",
-            imageRes = null,
+            imageRes = R.drawable.orig,
             isFavorite = true,
             phone = "+79655631039",
-            address = "Perm GGGGGGGGGGGG ddsa sdsdsd",
+            address = "г. Пермь, ул. Космонавта Беляева 61В, кв. 4",
             email = "MrMarwell@Gmail.com"
+        )
+    )
+}
+@Preview(showSystemUi = true)
+@Composable
+private fun MainPreviewNoData() {
+    ContactDetails(
+        contact = Contact(
+            name = "Евгений",
+            surname = "",
+            familyName = "Авдейкин",
+            imageRes = null,
+            isFavorite = false,
+            phone = "",
+            address = "г. Пермь, ул. Космонавта Беляева 61В, кв. 4",
+            email = null
         )
     )
 }
